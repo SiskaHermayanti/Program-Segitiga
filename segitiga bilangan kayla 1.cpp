8c155909c3996ea014d1a2ef1c64f1c436f2f04b
Siska Hermayanti <92504989+SiskaHermayanti@users.noreply.github.com>
2021-12-07 20:01:16 +0700

    Add files via upload

---
+++ segitiga bilangan kayla 1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+    cin >> n;
+    
+    cout << "";
+    
+    if(n>0 && n<10){
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+    }
+    else{
+        cout << "Bilangan tidak sesuai!";
+    }
+	
+	return 0;
+}
+
+
+
+	
+	
+
